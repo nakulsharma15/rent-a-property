cf846ff6d9fdcf85432d4961d5204567b8f3c433
nakulsharma15 <nakuls309@gmail.com>
2022-10-17 18:06:12 +0530

    feat: structured database

---
+++ src/database/propertiesData.js
@@ -0,0 +1,34 @@
+const propertiesData = [
+    {
+        name: "Aurora Studio",
+        image: "https://res.cloudinary.com/nakulsharma15/image/upload/v1666009311/Rent%20A%20Property/Delhi/home-design_scxw98.jpg",
+        rent: 4000,
+        location: "delhi",
+        availability: "immediately",
+        type: "studio",
+    },
+    {
+        name: "Galaxy Apartment",
+        image: "https://res.cloudinary.com/nakulsharma15/image/upload/v1666009176/Rent%20A%20Property/Delhi/download_rjlqw3.jpg",
+        rent: 9000,
+        location: "delhi",
+        availability: "within 1 month",
+        type: "apartment",
+    },
+    {
+        name: "Delight Height",
+        image: "https://res.cloudinary.com/nakulsharma15/image/upload/v1666009176/Rent%20A%20Property/Delhi/download_rjlqw3.jpg",
+        rent: 18000,
+        location: "delhi",
+        availability: "within 1 month",
+        type: "floor",
+    },
+    {
+        name: "Matrix Home",
+        image: "https://res.cloudinary.com/nakulsharma15/image/upload/v1666009097/Rent%20A%20Property/Delhi/download_d2mzcq.jpg",
+        rent: 29000,
+        location: "delhi",
+        availability: "after 3 month",
+        type: "house",
+    }
+];
